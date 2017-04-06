162f5e69531517e566b04af80ae2a1e75f5cbac9
Chuan Ning <cnin0770@Avalanche.local>
2017-04-06 14:14:06 +1000

    4th
    Fully functioning

--- AnnAnalyst.py
+++ AnnAnalyst.py
@@ -4,8 +4,13 @@ this is to calculate the key indicators such as precision and recall given the s
 
 
 import csv
+from collections import Counter
 
+user = 'cnin0770'
+inputfile = 'ann_edited.csv'
+outputfile = 'ind_cnin0770.csv'
 tt = 150
+raters = 55
 cats = [
     'Education',
     'Entertainment',
@@ -48,8 +53,11 @@ def readmatrix(individual):
             'Other': 0,
             'Error': 0
         }
+    # for cat in cats:
+    #     for dog in cats:
+    #         crs[cat].update({dog: 0})
 
-    with open('ann_edited.csv', "r", encoding='utf-8', errors='ignore') as ann:
+    with open(inputfile, "r", encoding='utf-8', errors='ignore') as ann:
         ann_csv = csv.reader(ann)
         heading = next(ann_csv)
         col_no = heading.index(individual)
@@ -67,7 +75,54 @@ def readmatrix(individual):
                 crs[row]['totalGold'] += crs[row][col]
                 crs[row]['totalPred'] += crs[col][row]
 
+    ann.close()
     return crs
+
+
+def flissmatrix():
+    flissM = []
+
+    with open(inputfile, "r", encoding='utf-8', errors='ignore') as ann:
+        ann_csv = csv.reader(ann)
+        for row in ann_csv:
+            rowed = row
+            if rowed:
+                rowed.pop(0)  # pop article names
+                rowed.pop(0)  # pop GOLD
+            flissM.append(Counter(rowed))
+
+    ann.close()
+    flissM.pop(0)  # pop heading
+
+    res = []
+    k = 0
+    pi = 0
+    pe = 0
+
+    for mat in flissM:
+        res.append([])
+        for cat in cats:
+            if cat in mat.keys():
+                res[k].append(mat[cat])
+            else:
+                res[k].append(0)
+        k += 1
+    del res[-1]  # remove last empty line caused by res.append([])
+
+    for i in range(len(res)):
+        for j in range(len(res[i])):
+            pi += (res[i][j] * res[i][j]) / ((raters - 1) * raters)
+
+        pi -= 1 / (raters - 1)
+    pi = pi / tt
+
+    for i in range(len(res[0])):
+        pj = 0
+        for j in range(len(res)):
+            pj += res[j][i] / (raters * tt)
+        pe += pj * pj
+
+    return (pi - pe) / (1 - pe)
 
 
 def analysing(matrix):
@@ -106,8 +161,42 @@ def analysing(matrix):
 
     return analyst
 
+
+def outputfun(res, outputf):
+    toWrite = [[]]
+    toWrite[0].append(user)
+    for key in res['Education'].keys():
+        toWrite[0].append(key)
+
+    k = 1
+    for cat in cats:
+        toWrite.append([])
+        toWrite[k].append(cat)
+        for value in res[cat].values():
+            toWrite[k].append(value)
+        k += 1
+
+    with open(outputf, 'w') as ed:
+        ed_csv = csv.writer(ed)
+        for l in toWrite:
+            ed_csv.writerow(l)
+
+    ed.close()
+    return True
+
+
+# def namelist():
+#     with open(inputfile, "r", encoding='utf-8', errors='ignore') as ann:
+#         ann_csv = csv.reader(ann)
+#         names = next(ann_csv)
+#     ann.close()
+#     names.pop()
+#     names.pop()
+#     return names
+
 if __name__ == "__main__":
-    user = 'cnin0770'
     result = analysing(readmatrix(user))
 
-    print(result['Entertainment'])
+    print('Fleiss\' kappa:', format(flissmatrix(), '3.2%'))
+
+    print(outputfun(result, outputfile))
--- AnnEditor.py
+++ AnnEditor.py
@@ -10,39 +10,46 @@ from collections import Counter
 lines = 8250
 units = 150
 
-with open('5046_w4_0328_annotations.csv', "r", encoding='utf-8', errors='ignore') as ann:
-    ann_csv = csv.reader(ann)
-    results = [[]]
-    k = 0
 
-    for i in range(0, units + 1):
-        results.append([])
-        for j in range(0, lines // units + 1):
-            results[i].append('')
+def getfile():
+    with open(infile, "r", encoding='utf-8', errors='ignore') as ann:
+        ann_csv = csv.reader(ann)
+        results = [[]]
+        k = 0
 
-    for row in ann_csv:
-        if row[0] != results[0][k // units]:
-            results[0][k // units + 1] = row[0]
+        for i in range(0, units + 1):
+            results.append([])
+            for j in range(0, lines // units + 1):
+                results[i].append('')
 
-        if row[1] != results[k % units]:
-            results[k % units + 1][0] = row[1]
+        for row in ann_csv:
+            if row[0] != results[0][k // units]:
+                results[0][k // units + 1] = row[0]
 
-        results[k % units + 1][k // units + 1] = row[2]
-        k += 1
+            if row[1] != results[k % units]:
+                results[k % units + 1][0] = row[1]
 
-    # for s in results:
-    #     print(len(s))
-    #
-    # print(len(results))
+            results[k % units + 1][k // units + 1] = row[2]
+            k += 1
+    ann.close()
 
-results[0].insert(1, 'GOLD')
+    results[0].insert(1, 'GOLD')
 
-for k in range(1, units + 1):
-    kCount = Counter(results[k])
-    results[k].insert(1, kCount.most_common(1)[0][0])
+    for k in range(1, units + 1):
+        kCount = Counter(results[k])
+        results[k].insert(1, kCount.most_common(1)[0][0])
+
+    return results
 
 
-with open('ann_edited.csv', 'w') as ed:
-    ed_csv = csv.writer(ed)
-    ed_csv.writerows(results)
+def givefile(results):
+    with open(outfile, 'w') as ed:
+        ed_csv = csv.writer(ed)
+        ed_csv.writerows(results)
+    ed.close()
+    return True
 
+if __name__ == "__main__":
+    outfile = 'ann_edited.csv'
+    infile = '5046_w4_0328_annotations.csv'
+    print(givefile(getfile()))
